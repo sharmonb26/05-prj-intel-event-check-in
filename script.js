1ac65e815bb836eb8b005d93dc26e37f68d2dacf
SeAirah Harmon-Brown <sharmonb@email.asu.edu>
2025-09-28 20:42:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -15,24 +15,9 @@ const attendeeList = document
   .getElementsByTagName("tbody")[0];
 const confettiContainer = document.getElementById("confetti");
 
-/ Check all needed DOM elements exist
-if (
-  !form ||
-  !nameInput ||
-  !teamSelect ||
-  !attendeeCount ||
-  !waterCount ||
-  !zeroCount ||
-  !powerCount ||
-  !greeting ||
-  !progressBar ||
+
   !document.getElementById("attendeeList") ||
   !document.getElementById("confetti")
-) {
-  console.log("One or more required elements are missing from the HTML.");
-  // Stop script if elements are missing
-  throw new Error("Missing DOM elements. Check your HTML IDs.");
-}
 
 // Track attendance
 let count = 0;
@@ -345,6 +330,7 @@ function showWinnerMessage() {
   localStorage.setItem("goalMessage", message);
   localStorage.setItem("winnerTeams", JSON.stringify(winners));
 }
+
 
 
 
